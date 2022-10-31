a484a58142d24c76bd98b54096728911c2407138
kellyannc <kelly.cunningham@cuny.edu>
2022-10-30 20:58:01 -0400

    CHANGES_Oct30_8:57PM

--- P5/scripts/main.js
+++ P5/scripts/main.js
@@ -45,7 +45,7 @@
 //   }
 // }
 
-// // ASSIGNMENT 7 EXERCISES
+// ASSIGNMENT 7 EXERCISES
 
 // function setup () {
 //     createCanvas(640, 360);
@@ -59,6 +59,9 @@
 //   ellipse(100, 400, 75, 125);
 //     rect(300, 200, 75, 150);
 // }
+
+
+// // ASSIGNMENT 7 Exercises -FACE
 
 // function setup () {
 //     createCanvas(640, 360);
@@ -78,7 +81,7 @@
 // rect(300, 200, 75, 150);
 // }
 
-// // ASSIGNMENT 7 IF THEN RETURN CERTAIN TEXT
+// ASSIGNMENT 7 IF THEN RETURN CERTAIN TEXT
 
 // function setup () {
 // let year = 2019;
@@ -90,27 +93,32 @@
 // } else if (year == 2017) {
 //   console.log("you're 4 years off my friend");
 // } 
-// // else if (year >= 2018) {
-// //   console.log('uh close enough');
-// // }
 // else if (year >= 2018) {
-//   console.log('idk man');
+//   console.log('uh close enough');
 // }
+// // else if (year >= 2018) {
+// //   console.log('idk man');
+// // }
 // }
 
+
+// ASSIGNMENT 7 IF THEN RETURN CERTAIN TEXT
 
 
 // function testFunction(p1, p2) {
 //   return p1 * p2;
 // }
+
+// // // below multiplies 5x6
 // let x = 5, y = 6;
 // let z = testFunction(5, 6)
 // console.log(z);
 
-// let x = 5;
-// let y = 6;
-// let z = x + y;
-// console.log(y)
+// // // below returns y which is 6
+// // let x = 5;
+// // let y = 6;
+// // let z = x + y;
+// // console.log(y)
 
 
 
@@ -194,11 +202,96 @@
 
 // }
 
-// // Assignment 8 GRASS CODE
+// // Assignment 8 GRASS CODE - Grass color changes randomly on refresh
 
 // This example draws a row of lines. The height and lean of each line is chosent at random to create a natural, grass-like appearance.
 
 // console.log("hello, grass");
+
+
+// let r, g, b;
+
+// function setup() {
+//   console.log("setup, grass");
+//   createCanvas(windowWidth, windowHeight);
+//   r = random(255);
+//   g = random(255);
+//   b = random(255);
+
+//   noStroke();
+//   // fill(255, 255, 255);
+
+//   noLoop();
+// }
+
+// function draw() {
+//   console.log("draw, grass");
+//   // background(40, 40, 40);
+
+//   fill(r, g, b);
+//   rect(0, height * 0.5, width, height * 0.5);
+
+//   stroke(r, g, b)
+//   // color(255, 0, 255);
+
+//   // drawGrass(0, height * 0.5 - 100, width, 300);
+//   // //stroke(240, 240, 240);
+//   drawGrass(10, height * 0.5 - 100, width, 100);
+//   //drawGrass(0, height * 0.5 - 100, width, 100);
+// }
+
+
+// function drawGrass(left, top, width, height) {
+//   // loop from the left to the right, one pixel per step
+//   for (let x = left; x < left + width; x++) {
+//     // x and y are the base of the blade of grass
+//     const y = top + height;
+
+
+//     // height of the grass
+//     // pick lowest of three "rolls" to bias strongly towards short blades
+//     // with occasional long ones
+//     const bladeHeight = min(
+//       random(200, height * .8),
+//       random(255, height * .5),
+//       random(50, height)
+//     );
+
+//     random(r)
+//     random(g)
+//     random(b);
+
+//     // horizontal offset of top of blade relative to base
+//     // average two "rolls" to bias towards middle
+//     let bladeLean = (random(-1, 1) + random(-1, 1)) * 0.5;
+    
+
+//     // scale lean by height so that shorter blades aren't more leany
+//     bladeLean = bladeLean * bladeHeight * .5;
+
+//     // scaling factor to to taste
+//     bladeLean = bladeLean * 1;
+
+//     // draw the grass
+//     line(x, y, x + bladeLean, y - bladeHeight)
+
+//   }
+// // // When the user clicks the mouse
+// // function mousePressed() {
+// //   // Check if mouse is inside the circle
+// //   let d = dist(mouseX, mouseY, 360, 200);
+// //   if (d < 100) {
+// //     // Pick new random color values
+// //     r = random(0);
+// //     g = random(0, 255);
+// //     b = random(0);
+// //   }
+// // }
+// }
+
+
+// // Assignment 8 GRASS CODE - each blade of grass with a randomly-chosen shade of green
+
 
 let r, g, b;
 
@@ -210,7 +303,7 @@ function setup() {
   b = random(255);
 
   noStroke();
-  // fill(255, 255, 255);
+  fill(255, 255, 255);
 
   noLoop();
 }
@@ -231,51 +324,39 @@ function draw() {
   //drawGrass(0, height * 0.5 - 100, width, 100);
 }
 
-
 function drawGrass(left, top, width, height) {
   // loop from the left to the right, one pixel per step
   for (let x = left; x < left + width; x++) {
     // x and y are the base of the blade of grass
     const y = top + height;
 
-
     // height of the grass
     // pick lowest of three "rolls" to bias strongly towards short blades
     // with occasional long ones
-    const bladeHeight = min(
-      random(200, height * .8),
-      random(255, height * .5),
-      random(50, height)
-    );
 
-    random(r)
-    random(g)
-    random(b);
+    
+    const bladeHeight = max(
+      random(0, height),
+      random(0, height),
+      random(0, height)
+    );
 
     // horizontal offset of top of blade relative to base
     // average two "rolls" to bias towards middle
-    let bladeLean = (random(-1, 1) + random(-1, 1)) * 0.5;
-    
-
+    let bladeLean = (random(-1, 1) + random(-1, 1)) * 3;
     // scale lean by height so that shorter blades aren't more leany
-    bladeLean = bladeLean * bladeHeight * .5;
-
+    bladeLean = bladeLean * bladeHeight;
     // scaling factor to to taste
-    bladeLean = bladeLean * 1;
+    bladeLean = bladeLean * 0.2;
 
-    // draw the grass
-    line(x, y, x + bladeLean, y - bladeHeight)
-
+    
+    ////
+    //CHANGE THE COLOR OF THE GRASS
+    ////
+		let randomGreen = color(0, random(255), 0);
+    stroke(randomGreen);
+    line(x, y, x + bladeLean, y - bladeHeight);
   }
-// // When the user clicks the mouse
-// function mousePressed() {
-//   // Check if mouse is inside the circle
-//   let d = dist(mouseX, mouseY, 360, 200);
-//   if (d < 100) {
-//     // Pick new random color values
-//     r = random(0);
-//     g = random(0, 255);
-//     b = random(0);
-//   }
-// }
 }
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,39 @@ function draw () {
 }
 console.log('blah')
 
+
+
+function drawGrass(left, top, width, height) {
+    // loop from the left to the right, one pixel per step
+    for (let x = left; x < left + width; x++) {
+      // x and y are the base of the blade of grass
+      const y = top + height;
+  
+      // height of the grass
+      // pick lowest of three "rolls" to bias strongly towards short blades
+      // with occasional long ones
+  
+      
+      const bladeHeight = max(
+        random(0, height),
+        random(0, height),
+        random(0, height)
+      );
+  
+      // horizontal offset of top of blade relative to base
+      // average two "rolls" to bias towards middle
+      let bladeLean = (random(-1, 1) + random(-1, 1)) * 3;
+      // scale lean by height so that shorter blades aren't more leany
+      bladeLean = bladeLean * bladeHeight;
+      // scaling factor to to taste
+      bladeLean = bladeLean * 0.2;
+  
+      
+      ////
+      //CHANGE THE COLOR OF THE GRASS
+      ////
+          let randomGreen = color(0, random(255), 0);
+      stroke(randomGreen);
+      line(x, y, x + bladeLean, y - bladeHeight);
+    }
+  }
