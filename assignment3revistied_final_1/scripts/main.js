6863d2be7caa43a27e14884be1dac877a5574914
kellyannc <kelly.cunningham@cuny.edu>
2022-12-10 19:57:50 -0500

    CHANGES_121022_8PM

--- assignment3revistied_final_1/scripts/main.js
+++ assignment3revistied_final_1/scripts/main.js
@@ -1,3 +1,9 @@
+newdiv = document.createElement('div');
+document.querySelector('body').appendChild(newdiv);
+console.log('HI IM WORKING');
 
+//  function myFunction() {
+//     alert("1974: David Bowie's persona of Halloween Jack during the Diamond Dog album creation.");
+//   }
 
   
